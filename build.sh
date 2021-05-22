8df608e3626407e1d7bdaffbc8ce5852d993bd67
kusa-mochi <whoatemyapplepie@gmail.com>
2021-05-22 22:41:41 +0900

    fix the shell to build source.

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 # Run this shell and upload the zip file to AWS lambda.
 # 2021/5/22 mochi
 
+srcDir=./
 distDir=./dist
 
 [[ -d $distDir ]] && rm -R $distDir
 mkdir $distDir
-GOOS=linux GOARCH=amd64 go build -o $distDir/NextReversiMove
-zip $distDir/NextReversiMove.zip $distDir/NextReversiMove
+GOOS=linux GOARCH=amd64 go build -o NextReversiMove
+zip $distDir/NextReversiMove.zip NextReversiMove
+rm NextReversiMove
 
 exit 0
